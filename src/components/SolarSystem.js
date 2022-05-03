edabab4dbe34d99d3b6e9478da71cc6f8b44f976
GiselyKC <gikavalli@gmail.com>
2022-05-03 17:39:44 -0300

    acrescentado o import, div e className para o css

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -4,21 +4,25 @@ import PlanetCard from './PlanetCard';
 import Title from './Title';
 import planets from '../data/planets';
 
+import './SolarSystem.css';
+
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        {
-          planets
-            .map(({ name, image }) => (
-              <PlanetCard
-                key={ name } // exigencia do react, para diferenciar uma propriedade da outra.
-                planetName={ name }
-                planetImage={ image }
-              />
-            ))
-        }
+        <div className="solar-system">
+          {
+            planets
+              .map(({ name, image }) => (
+                <PlanetCard
+                  key={ name } // exigencia do react, para diferenciar uma propriedade da outra.
+                  planetName={ name }
+                  planetImage={ image }
+                />
+              ))
+          }
+        </div>
       </div>
     );
   }
