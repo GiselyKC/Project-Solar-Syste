9d698b5b3a6f0565e776925ba0e97915e0903f01
GiselyKC <gikavalli@gmail.com>
2022-05-03 17:35:10 -0300

    acrescentado o className e o import no arq. MissionsCard.js

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 
+import './Missions.css';
+
 class MissionCard extends Component {
   render() {
     const { name, year, country, destination } = this.props;
 
     return (
-      <div data-testid="mission-card">
+      <div className="card" data-testid="mission-card">
         <p data-testid="mission-name">{ name }</p>
         <p data-testid="mission-year">{ year }</p>
         <p data-testid="mission-country">{ country }</p>
