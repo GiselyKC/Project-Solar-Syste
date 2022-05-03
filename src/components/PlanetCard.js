cf530a86963867c82ea083ab452d1995c65743f0
GiselyKC <gikavalli@gmail.com>
2022-05-03 17:38:15 -0300

    Acrescentado o import e o className para o css

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 
+import './PlanetCard.css';
+
 class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
 
     return (
-      <div data-testid="planet-card">
+      <div className="planet-card" data-testid="planet-card">
         <p data-testid="planet-name" key={ planetName }>{ planetName }</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } key={ planetName } />
       </div>
