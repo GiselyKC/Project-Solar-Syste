be4c688a8a6ec188fc6bb5be2c0be577dc8b9c4d
GiselyKC <gikavalli@gmail.com>
2022-05-03 17:32:03 -0300

    acrescentado o className no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
 
+import './App.css';
+
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Header />
         <SolarSystem />
         <Missions />
